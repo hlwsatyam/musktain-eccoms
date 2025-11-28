0e2838fa408d1ee180bd49cf086ac0b93c727fe7
hlwsatyam <satyampandit021@gmail.com>
2025-11-28 22:04:17 +0530

    added api

--- frontend/src/helper/l1.js
+++
@@ -1,6 +0,0 @@
-export const baseUrl ="http://localhost:3100"
-export const SHIPPING_FEE = 0
-
-export const CLOUDINARY_SECRET_KEY="GZq786wGiQ7GHvsbNaSJx3rKpu0"
-export const CLOUDINARY_CLOUD_NAME="dikxwu8om"
-export const CLOUDINARY_PUBLISHABLE_KEY="458816267734883"
